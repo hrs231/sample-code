ab58758d4aa7065b7d13cde288f5808961980a4d
hrs231 <henrysimpson@gmail.com>
2019-09-24 14:53:48 +0800

    updated generic logging class to create log directory

--- floyd_warshall/common/loggable.py
+++ floyd_warshall/common/loggable.py
@@ -1,8 +1,11 @@
+import os
 import logging
 
 
 class Loggable(object):
-    """ Generic Logging helper class """
+    """ Generic Logging helper class
+        defaults to creating new log directory ./logs/
+        unless LOG_DIR environment variable set  """
 
     def __init__(self):
         self.logger_active = False
@@ -10,7 +13,10 @@ class Loggable(object):
 
     def init_logging(self, logger_name):
         self.logger = logging.getLogger(logger_name)
-        logfile = "logs/" + logger_name + ".log"
+        logging_directory = os.environ.get("LOG_DIR", "logs")
+        if not os.path.exists(logging_directory):
+            os.makedirs(logging_directory)
+        logfile = os.path.join(logging_directory, logger_name + ".log")
         hdlr = logging.FileHandler(logfile)
         hdlr.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s: %(message)s'))
         self.logger.addHandler(hdlr)
